2815966f6ee97c4190bf47fcf4fe86e38c492cd4
najimiman <114479872+najimiman@users.noreply.github.com>
2023-02-06 09:55:42 +0100

    outoformation kotlin

---
+++ MyFirst/lib/src/main/java/com/example/lib/MyClass.kt
@@ -0,0 +1,10 @@
+package com.example.lib
+
+fun main() {
+    println("hello")
+    var a:String="iman"
+    println(a)
+    println("le nom=")
+    var b:String= readLine()!!
+    println("votre nom est:$b")
+}
