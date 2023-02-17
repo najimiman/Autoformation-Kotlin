ec9a1775935f34c4b17ef4088f9e4d489dd1b04c
najimiman <114479872+najimiman@users.noreply.github.com>
2023-02-17 13:39:28 +0100

    role image

--- Dice-Roller/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Dice-Roller/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,15 +18,35 @@ class MainActivity : AppCompatActivity() {
         //textView1.setText("6")
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
         Roledice()
+
     }
 
+
+
     private fun Roledice() {
-        val dice=Dice(10)
+        val dice=Dice(6)
         var dicerole=dice.roll()
         var textView1=findViewById<TextView>(R.id.textView)
         textView1.setText(dicerole.toString())
-        //var textView1:TextView=findViewById(R.id.textView)
-        //textView1.text=dicerole.toString()
+        var imageview:ImageView=findViewById(R.id.imageView)
+      /* when(dicerole){
+           1->imageview.setImageResource(R.drawable.dice_1)
+           2->imageview.setImageResource(R.drawable.dice_2)
+           3->imageview.setImageResource(R.drawable.dice_3)
+           4->imageview.setImageResource(R.drawable.dice_4)
+           5->imageview.setImageResource(R.drawable.dice_5)
+           6->imageview.setImageResource(R.drawable.dice_6)
+
+       }*/
+        val drawableResource = when (dicerole) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+        imageview.setImageResource(drawableResource)
     }
 
     class Dice(val num:Int){
